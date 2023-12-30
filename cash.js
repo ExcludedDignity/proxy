798ab0f67ed6624138eb07de1ad70bef20c5abc3
ExcludedDignity <149678884+ExcludedDignity@users.noreply.github.com>
2023-12-31 01:20:33 +1000

    Create cash.js
    Created cash logger for ngrok code

---
+++ cash.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const axios = require('axios');
+const app = express();
+
+app.use(express.json());
+
+app.post('/forward-webhook', async (req, res) => {
+    try {
+        const response = await axios.post('webhook removed', req.body);
+        res.send(response.data);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send(error.toString());
+    }
+});
+
+const PORT = process.env.PORT || 2990;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
